f8a46045f593e452f2c1f06cad581f1c62d9042f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-08-06 07:31:34 +0530

    Handling Dynamic Data

--- src/components/RestrauntCard.jsx
+++ src/components/RestrauntCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const RestrauntCard = (props) => {
+    // console.log(props);
+    const imageURL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + props.resData.info.cloudinaryImageId;
     return (
         <div className='resCard'>
-            <img className='resImage' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/8/98c26105-cf68-44ea-a098-d942eb4aa894_866042.jpg" alt="No Image Found" />
-            <h3 className='resName'>{props.resName}</h3>
-            <h4>Biryani, North India</h4>
-            <h4>4.4 Stars</h4>
+            <img className='resImage' src={imageURL} alt="No Image Found" />
+            <h3 className='resName'>{props.resData.info.name}</h3>
+            <h4>{props.resData.info.cuisines.join(", ")}</h4>
+            <h4>{props.resData.info.avgRating}</h4>
         </div>
     )
 }
