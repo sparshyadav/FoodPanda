61ec17af800e2423569575ba2d794a979249d14f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-08-06 07:38:31 +0530

    Props Destructuring Done

--- src/components/RestrauntCard.jsx
+++ src/components/RestrauntCard.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const RestrauntCard = (props) => {
     // console.log(props);
+    const { name, cuisines, avgRating } = props.resData.info;
     const imageURL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + props.resData.info.cloudinaryImageId;
     return (
         <div className='resCard'>
-            <img className='resImage' src={imageURL} alt="No Image Found" />
-            <h3 className='resName'>{props.resData.info.name}</h3>
-            <h4>{props.resData.info.cuisines.join(", ")}</h4>
-            <h4>{props.resData.info.avgRating}</h4>
+            <img className='resImage' src={imageURL} alt="No  Image Found" />
+            <h3 className='resName'>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating}</h4>
         </div>
     )
 }
